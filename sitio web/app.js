c2c9c87fa8f13e3e6a5c8b7f35c333c070012060
CarolinaShlain123 <caroshlain@gmail.com>
2022-07-03 22:34:37 -0300

    cambiando a ejs

--- sitio web/app.js
+++ sitio web/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 // esta porción de código hay que poner para traer el modulo de ruta y asi vincular controlador// 
 const mainRoute =require('./routes/mainRoute')
 
@@ -17,34 +19,31 @@ app.listen(process.env.PORT || 3000, () => console.log('Server runnning'));
 ////mejorar este código con propuesta debajo///
 app.use('/public', express.static(__dirname + '/public'));
 
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.ejs'));
+  res.render('home');
 });
 
-app.get('/home.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.ejs'));
+app.get('/', (req, res) => {
+  res.render('cart');
 });
 
-app.get('/register.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/register.ejs'));
+app.get('/', (req, res) => {
+  res.render('login');
 });
 
-app.get('/login.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.htmlejs'));
+app.get('/', (req, res) => {
+  res.render('producto');
 });
 
-app.get('/producto.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/producto.ejs'));
-  });
-
-
-
-
+app.get('/', (req, res) => {
+  res.render('register');
+});
 
   /* Nuevo código código MVC. - creo estructura de carpetas y controlador + archivo rutas
 
 
-app.use(express.static('./public'))
+
 
 app.set('view engine', 'ejs');
 
