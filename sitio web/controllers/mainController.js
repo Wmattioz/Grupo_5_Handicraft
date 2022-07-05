f5a144b5bb3c113f2965c5673a6a8cfa309e49ec
CarolinaShlain123 <caroshlain@gmail.com>
2022-07-05 13:33:18 -0300

    .

--- sitio web/controllers/mainController.js
+++ sitio web/controllers/mainController.js
@@ -1,7 +1,22 @@
 const mainController = {
 
     mostrarHome: (req,res) =>{
-        return res.render('home')
+        return res.render('home') 
+        /*,{
+            articles:[{
+                title: 'articulo 1',
+                img: "public/images/H5204c57802c84d74b06b98bcf45bfb5ag.png",
+                precio: '$12000',
+                descripcion: 'Alzada con pie en forma de flor'
+            },
+            {
+                title: 'articulo 1',
+                img: "public/images/H5204c57802c84d74b06b98bcf45bfb5ag.png",
+                precio: '$12000',
+                descripcion: 'Alzada con pie en forma de flor'
+            }
+        ]
+        }*/
     },
     
 
