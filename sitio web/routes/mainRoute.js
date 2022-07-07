066f09c27cba7750e8d8b3866b99fdb0b5125688
fcgrandoli <82011284+fcgrandoli@users.noreply.github.com>
2022-07-07 12:17:26 -0300

    Agregadon archivos con rutas individuales.

--- sitio web/routes/mainRoute.js
+++ sitio web/routes/mainRoute.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const controllerHome = require('../controllers/controllerHome');
+const controllerRegister = require('../controllers/controllerRegister');
+const controllerLogin = require('../controllers/controllerLogin');
+const controllerProducto = require('../controllers/controllerProducto');
+const controllerCart = require('../controllers/controllerCart');
+const controllerConstruccion = require('../controllers/controllerConstruccion');
 
-const mainController = require('../controllers/mainController');
+//const mainController = require('../controllers/mainController');
+//const { mostrarHome } = require('../controllers/controllerHome');
 
-router.get('/', mainController.mostrarHome);
+router.get('/', controllerHome.mostrarHome);
 
-router.get('/home', mainController.mostrarHome);
+router.get('/home', controllerHome.mostrarHome);
 
-router.get('/register', mainController.mostrarRegister);
+router.get('/register', controllerRegister.mostrarRegister);
 
-router.get('/login', mainController.mostrarLogin);
+router.get('/login', controllerLogin.mostrarLogin);
 
-router.get('/producto', mainController.mostrarProducto);
+router.get('/producto', controllerProducto.mostrarProducto);
 
-router.get('/cart', mainController.mostrarCart);
+router.get('/cart', controllerCart.mostrarCart);
 
-router.get('/enConstruccion', mainController.mostrarConstruccion);
+router.get('/enConstruccion', controllerConstruccion.mostrarConstruccion);
 
 module.exports = router;
