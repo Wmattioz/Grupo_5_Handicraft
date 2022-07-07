130cea6cc5a918c3c277651b51959fe978f2aae7
fcgrandoli <82011284+fcgrandoli@users.noreply.github.com>
2022-07-07 13:28:37 -0300

    Actualizando a rutas MVC

--- sitio web/app.js
+++ sitio web/app.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const app = express();
-const mainRoute =require('./routes/mainRoute')
+const cartRoute = require('./routes/cartRoute.js');
+const constructRoute = require('./routes/constructRoute.js');
+const homeRoute = require('./routes/homeRoute.js');
+const loginRoute = require('./routes/loginRoute.js');
+const productRoute = require('./routes/productRoute.js');
+const registerRoute = require('./routes/registerRoute.js');
 const path = require('path');
-const multer = require ('multer');
+const multer = require('multer');
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
+
 app.listen(process.env.PORT || 3000, () => console.log('Server runnning'));
 
+app.use('/', homeRoute);
+app.use('/home', homeRoute);
+app.use('/login', loginRoute);
+app.use('/register', registerRoute);
+app.use('/producto', productRoute);
+app.use('/cart', cartRoute);
 
+// DIVISOR ========================================================================
 
-
-app.use('/', mainRoute);
+//const mainRoute =require('./routes/mainRoute.old')
+/* app.use('/', mainRoute);
 app.use('/login', mainRoute);
 app.use('/register', mainRoute);
 app.use('/producto', mainRoute);
-app.use('/cart', mainRoute);
+app.use('/cart', mainRoute); */
 
-
-
-  /* Nuevo código código MVC. - creo estructura de carpetas y controlador + archivo rutas
-
-
-
+/* Nuevo código código MVC. - creo estructura de carpetas y controlador + archivo rutas
 
 app.set('view engine', 'ejs');
 
