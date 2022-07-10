7f068844a9fe37e240476306c62273f8567a0f67
fcgrandoli <82011284+fcgrandoli@users.noreply.github.com>
2022-07-10 09:58:15 -0300

    Agregando usuarios

---
+++ sitio web/views/users/usersList.js
@@ -0,0 +1,42 @@
+const userList = [
+  {
+    id: 1,
+    first_name: 'Carolina',
+    last_name: 'Shlain',
+    user_name: 'cshlain',
+    role: 'admin',
+  },
+  {
+    id: 2,
+    first_name: 'Shirel',
+    last_name: 'Lalo',
+    user_name: 'slalo',
+    role: 'admin',
+  },
+  {
+    id: 3,
+    first_name: 'Walter',
+    last_name: 'Mattioz',
+    user_name: 'wmattioz',
+    role: 'admin',
+  },
+  {
+    id: 4,
+    first_name: 'Fernando',
+    last_name: 'Grandoli',
+    user_name: 'fgrandoli',
+    role: 'admin',
+  },
+];
+
+module.exports = userList;
+
+/* TEMPLATE:
+
+const products = {
+    id: #,
+    first_name: '',
+    last_name: '',
+    role: '',
+    }
+    */
